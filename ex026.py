92d99af29943976c63097aa26275b768669c5339
Gustavo Bernardes da Silva <gustavobersilva@gmail.com>
2022-11-29 18:28:02 -0300

    Update ex026.py

--- ex026.py
+++ ex026.py
@@ -1,3 +1,9 @@
+"""
+
+Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
+
+"""
+
 nome = str(input('Digite sem nome completo: ')).strip()
 
 lista = nome.split()
