516ae4675f68dbbe4e266a1dc5929775d8f96a7f
Gustavo Bernardes da Silva <gustavobersilva@gmail.com>
2022-11-29 18:17:46 -0300

    Update ex007.py

--- ex007.py
+++ ex007.py
@@ -1,3 +1,9 @@
+"""
+
+Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+
+"""
+
 nota1 = float(input('Digite a primeira nota: '))
 nota2 = float(input('Digite a segunda nota: '))
 
