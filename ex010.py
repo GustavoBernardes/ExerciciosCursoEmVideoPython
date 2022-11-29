09554647de5845a5a583adcac58ee21736205fe5
Gustavo Bernardes da Silva <gustavobersilva@gmail.com>
2022-11-29 18:19:20 -0300

    Update ex010.py

--- ex010.py
+++ ex010.py
@@ -1,3 +1,9 @@
+"""
+
+Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
+
+"""
+
 reais = float(input('Digite o valor que deseja converter: R$'))
 
 print(f'R${reais} convertido em dolares: {(reais/5.33):.2f}')
