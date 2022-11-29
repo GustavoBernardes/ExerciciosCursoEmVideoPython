8d86b8447b94dac069b09134d755bdf8ea2e30fa
Gustavo Bernardes da Silva <gustavobersilva@gmail.com>
2022-11-29 18:21:15 -0300

    Update ex013.py

--- ex013.py
+++ ex013.py
@@ -1,3 +1,9 @@
+"""
+
+Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+
+"""
+
 salario = float(input('Digite o salario: R$'))
 
 acrescimo = salario + (salario * 0.15)
