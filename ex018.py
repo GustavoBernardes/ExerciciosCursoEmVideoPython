a6d0eefe0d859433cf8c4166e664f4a43f27e404
Gustavo Bernardes da Silva <gustavobersilva@gmail.com>
2022-11-29 18:24:15 -0300

    Update ex018.py

--- ex018.py
+++ ex018.py
@@ -1,3 +1,9 @@
+"""
+
+Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
+
+"""
+
 import math
 
 angulo = float(input('Digite um angulo: '))
