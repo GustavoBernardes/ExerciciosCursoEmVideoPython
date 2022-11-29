86faa94ab21960d0c1ae3e5bfa83abf9ae5ba0c3
Gustavo Bernardes da Silva <gustavobersilva@gmail.com>
2022-11-29 18:26:06 -0300

    Update ex022.py

--- ex022.py
+++ ex022.py
@@ -1,3 +1,9 @@
+"""
+
+Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.
+
+"""
+
 num = int(input('Digite um número de 0 a 9999: '))
 
 unidade = num // 1 % 10
