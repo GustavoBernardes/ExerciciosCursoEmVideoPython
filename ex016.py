32ca9177ef697346c952a35cb3dfbd133400e16c
Gustavo Bernardes da Silva <gustavobersilva@gmail.com>
2022-11-29 18:23:09 -0300

    Update ex016.py

--- ex016.py
+++ ex016.py
@@ -1,3 +1,9 @@
+"""
+
+Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
+
+"""
+
 from math import ceil, floor, trunc
 
 num = float(input('Digite um número real: '))
