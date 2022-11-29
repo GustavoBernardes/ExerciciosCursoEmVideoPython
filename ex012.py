cdbdc6af25aabf16ed21b3b8fc2030103818cdc7
Gustavo Bernardes da Silva <gustavobersilva@gmail.com>
2022-11-29 18:20:45 -0300

    Update ex012.py

--- ex012.py
+++ ex012.py
@@ -1,3 +1,9 @@
+"""
+
+Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
+
+"""
+
 preco = float(input('Digite o valor do produto: R$'))
 
 desconto = preco - (preco * 0.05)
